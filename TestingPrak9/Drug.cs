d3d6d52a548aab9227cdb7d4af9f4810ab62a081
1318PK <ainur.hak2004@gmail.com>
2022-05-18 14:59:24 +0300

    Добавьте файлы проекта.

---
+++ TestingPrak9/Drug.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPrak9
+{
+    public class Drug
+    {
+        public string Name { get; set; }
+        public string Producer { get; set; }
+        public double Price { get; set; }
+    }
+}
